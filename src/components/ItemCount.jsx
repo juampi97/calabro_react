8f5ed278b5d55f0aeac59941594a9428c5eb57cd
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-02-16 21:54:56 -0300

    Item count

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemCount = () => {
+  return (
+    <div className='text-center'>ItemCount</div>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({ greeting }) => {
   return (
     <>
       <div>
         <h3 className="text-center pt-3">{greeting}</h3>
+
+        <div className="container-fluid d-flex align-items-center justify-content-around my-4">
+          <div className="col-2 py-2 border border-2 border-primary rounded-pill">
+            <ItemCount />
+          </div>
+        </div>
       </div>
     </>
   );
