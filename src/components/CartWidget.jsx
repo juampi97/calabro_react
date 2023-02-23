38ffe068173d29b165f1ceae1a4125c63b4072e3
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-02-23 12:34:03 -0300

    Promises

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -7,11 +7,21 @@ import Button from "react-bootstrap/Button";
 const CartWidget = () => {
   return (
     <>
-      <Button className="d-flex align-items-center justify-content-around ms-2 " variant="success">
-        <span className="material-symbols-outlined me-1">shopping_cart</span>
-        <Badge bg="warning">
-          3
-        </Badge>
+      <Button
+        className="d-flex align-items-center justify-content-around ms-2 "
+        variant="success"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          className="bi bi-cart2"
+          viewBox="0 0 20 20"
+        >
+          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+        </svg>
+        <Badge bg="warning">3</Badge>
         <span className="visually-hidden">Productos carrito</span>
       </Button>
     </>
