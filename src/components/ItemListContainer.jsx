d070a3372b16df790b3c4b91c0768e9eaee0cec4
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-02-28 13:11:48 -0300

    Item list

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Import Hooks
+import { useState, useEffect } from "react";
+
 // Import components propios
 import ItemList from "./ItemList";
 
@@ -7,12 +10,37 @@ import ItemList from "./ItemList";
 import Data from "../data.json";
 
 const ItemListContainer = ({ greeting }) => {
+  // const fechData = async () => {
+  //   const response = await fetch('../data.json');
+  //   const data = await response.json();
+  //   return data;
+  // };
 
-  return (
-    <>
-      <ItemList productos={Data} />
-    </>
-  );
+  // const [productos,setProductos] = useState([]);
+
+  // useEffect(() => {
+  //   fechData().then((data) => setProductos(data));
+  // }, []);
+
+  const getData = () => {
+    return new Promise((resolve, reject) => {
+      if (Data.length === 0) {
+        reject(new Error("No hay productos"));
+      }
+      setTimeout(() => {
+        resolve(Data);
+      }, 2000);
+    });
+  };
+
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+  getData().then((data) => setProductos(data));
+  getData().catch((error) => console.log(error));
+  },[]);
+
+  return <>{ <ItemList productos={productos} /> }</>;
 };
 
 export default ItemListContainer;
