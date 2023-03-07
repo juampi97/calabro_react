ddd4e38e627518d28288c1e7739feecefd130947
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-03-07 12:56:40 -0300

    funcion onAddCart

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 // Import hook
 import { useState } from "react";
-
+import { useParams } from "react-router-dom";
 // Import components bootstrap
 import Button from "react-bootstrap/Button";
 
 const ItemCount = ({ stock }) => {
+
+  const { cod_rec } = useParams();
+
   const cantidadInicial = 1;
   const [cantidad, setCantidad] = useState(cantidadInicial);
 
@@ -14,26 +17,38 @@ const ItemCount = ({ stock }) => {
     cantidad < stock ? setCantidad(cantidad + 1) : setCantidad(cantidad);
   };
   const onRestar = () => {
-    cantidad > cantidadInicial ? setCantidad(cantidad - 1) : setCantidad(cantidad);
+    cantidad > cantidadInicial
+      ? setCantidad(cantidad - 1)
+      : setCantidad(cantidad);
+  };
+  const onAddCart = () => {
+    console.log(`Se agregaron ${cantidad} unidades al carrito del producto ${cod_rec}`);
   };
 
   return (
     <>
-      <Button
-        variant="outline-light"
-        className="d-flex align-items-center justify-content-around text-dark"
-        onClick={onRestar}
-      >
-        <span className="material-symbols-outlined">horizontal_rule</span>
-      </Button>
-      <div className="col-6 d-flex justify-content-around">{cantidad}</div>
-      <Button
-        variant="outline-light"
-        className="d-flex align-items-center justify-content-around text-dark"
-        onClick={onAdd}
-      >
-        <span className="material-symbols-outlined">add</span>
-      </Button>
+      <div className="d-flex flex-row align-items-center justify-content-around col-10 col-md-5 col-lg-9 my-lg-3 p-2 border border-2 border-light-subtle rounded-3">
+        <Button
+          variant="outline-light"
+          className="d-flex align-items-center justify-content-around text-dark"
+          onClick={onRestar}
+        >
+          <span className="material-symbols-outlined">horizontal_rule</span>
+        </Button>
+        <div className="col-6 d-flex justify-content-around">{cantidad}</div>
+        <Button
+          variant="outline-light"
+          className="d-flex align-items-center justify-content-around text-dark"
+          onClick={onAdd}
+        >
+          <span className="material-symbols-outlined">add</span>
+        </Button>
+      </div>
+      <div className="pt-2">
+        <Button onClick={onAddCart} className="py-2 p-3" variant="warning">
+          Agregar
+        </Button>
+      </div>
     </>
   );
 };
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-// Import Bootstrap components
-import Button from "react-bootstrap/Button";
 // Import components
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ cod_rec, marca, modelo, hdmi, vga, stock }) => {
-
   const salidaHdmi = hdmi ? "Disponible" : "No disponible";
   const salidaVga = vga ? "Disponible" : "No disponible";
 
@@ -15,7 +12,11 @@ const ItemDetail = ({ cod_rec, marca, modelo, hdmi, vga, stock }) => {
     <>
       {/* Imagen producto segun prop */}
       <div className="col-10 col-lg-5 d-flex flex-column align-items-center justify-content-center mb-4 mb-lg-0">
-        <img  src={ `/productos/${idImg}.jpg` } alt="Imagen producto" className="img-fluid" />
+        <img
+          src={`/productos/${idImg}.jpg`}
+          alt="Imagen producto"
+          className="img-fluid"
+        />
       </div>
       {/* Detalle producto */}
       <div className="col-11 col-lg-4 d-flex flex-column align-items-center justify-content-start p-3 border border-2 border-light-subtle rounded-3">
@@ -23,14 +24,7 @@ const ItemDetail = ({ cod_rec, marca, modelo, hdmi, vga, stock }) => {
         <h4 className="mt-1 mt-lg-3 mb-2 mb-lg-0"> {marca}</h4>
         <p className="mt-1 mt-lg-3 mb-2 mb-lg-0">Salida HDMI: {salidaHdmi}</p>
         <p className="mt-1 mt-lg-2 mb-2 mb-lg-2">Salida Vga: {salidaVga}</p>
-        <div className="d-flex flex-row align-items-center justify-content-around col-10 col-md-8 my-lg-3 p-2 border border-2 border-light-subtle rounded-3">
-          <ItemCount stock={stock} />
-        </div>
-        <div className="pt-2">
-          <Button className="py-2 p-3" variant="warning">
-            Agregar
-          </Button>
-        </div>
+        <ItemCount stock={stock} />
       </div>
     </>
   );
